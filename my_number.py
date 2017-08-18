b4283aac8c69cd641b62de357672559dabba4f07
Yoshimura Yuu <yyu@mental.poker>
2017-08-19 04:41:43 +0900

    fix

--- my_number.py
+++ my_number.py
@@ -9,17 +9,18 @@ if __name__ == "__main__":
     try:
         mother = int(sys.argv[1])
         father = int(sys.argv[2])
+        env    = [] if len(sys.argv) <= 3 else map(int, sys.argv[3].split(","))
     except Exception:
-        print "usage: python my_number.py {mother's number} {father's number}"
+        print "usage: python my_number.py {mother's number} {father's number} {env}"
         sys.exit(0)
     
-    print "mother: %s, father %s" % (mother, father)
+    print "mother: %s, father: %s, env: %s" % (mother, father, env)
 
     p = sympy.nextprime(max(mother, father))
     my_number = 0
     while True:
         my_number = 2 * mother * father * p + 1
-        if (p > father) and sympy.isprime(my_number):
+        if p not in env and sympy.isprime(my_number):
             break
         else:
             p = sympy.nextprime(p)
